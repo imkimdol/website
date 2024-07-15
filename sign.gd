bfa2982d435ac1f9d7bf4d558862885c525cec6c
imkimdol <kimdol@proton.me>
2024-07-15 10:08:00 -0700

    Change sign behaviour

--- sign.gd
+++ sign.gd
@@ -1,6 +1,19 @@
 class_name Sign extends StaticBody2D
 
+@export var text := "Text goes here!"
+
 var player_entered := false
+
+func _ready():
+	$InteractionLabel.modulate = Color(1,1,1,0)
+	
+	# https://forum.godotengine.org/t/changing-the-background-color-of-a-label/3006/2
+	var new_sb = StyleBoxFlat.new()
+	new_sb.bg_color = Color(0,0,0,0.5)
+	
+	var label = $InteractionLabel/HBoxContainer/Label
+	label.text = text
+	label.add_theme_stylebox_override("normal", new_sb)
 
 func _process(delta):
 	if Input.is_action_pressed("ui_accept") and player_entered:
@@ -12,7 +25,6 @@ func on_sign_interact():
 func _on_area_2d_body_entered(body):
 	player_entered = true
 	Global.focus_camera.emit()
-	$Sprite2D.modulate = Color(0.5, 0.5, 1)
 	
 	var tween = get_tree().create_tween()
 	tween.tween_property($InteractionLabel, "modulate", Color(1,1,1,1), 0.5)
@@ -20,7 +32,6 @@ func _on_area_2d_body_entered(body):
 func _on_area_2d_body_exited(body):
 	player_entered = false
 	Global.unfocus_camera.emit()
-	$Sprite2D.modulate = Color(1, 1, 1)
 	
 	var tween = get_tree().create_tween()
 	tween.tween_property($InteractionLabel, "modulate", Color(1,1,1,0), 0.5)
