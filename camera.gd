c20788b7e010ce374988ae32f0c1393e481d46d2
imkimdol <kimdol@proton.me>
2024-07-15 10:48:11 -0700

    Refactor and adjust camera zoom

--- camera.gd
+++ camera.gd
@@ -1,15 +1,20 @@
 extends Camera2D
 
+var tween: Tween
+
 func _ready():
 	Global.focus_camera.connect(on_focus_camera)
 	Global.unfocus_camera.connect(on_unfocus_camera)
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "zoom", Vector2(1.75, 1.75), 2)
+	zoom_to(Vector2(2, 2), 2)
 
 func on_focus_camera():
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "zoom", Vector2(2.5, 2.5), 0.5)
+	zoom_to(Vector2(3, 3), 0.5)
 
 func on_unfocus_camera():
-	var tween = get_tree().create_tween()
-	tween.tween_property(self, "zoom", Vector2(1.75, 1.75), 0.5)
+	zoom_to(Vector2(2, 2), 1)
+
+func zoom_to(amount: Vector2, duration: float):
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween()
+	tween.tween_property(self, "zoom", amount, duration)
