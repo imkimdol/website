fe68965609f0b40f17c548f123b69ce8c10f1696
imkimdol <kimdol@proton.me>
2024-07-14 20:17:36 -0700

    Add WASD to move indicator and camera zoom in

--- camera.gd
+++ camera.gd
@@ -3,6 +3,8 @@ extends Camera2D
 func _ready():
 	Global.focus_camera.connect(on_focus_camera)
 	Global.unfocus_camera.connect(on_unfocus_camera)
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "zoom", Vector2(1.75, 1.75), 2)
 
 func on_focus_camera():
 	var tween = get_tree().create_tween()
--- player.gd
+++ player.gd
@@ -4,18 +4,42 @@ const ACCELERATION = 100.0
 const DECELLERATION = 50.0
 const MAX_SPEED = 300.0
 
+var seconds_from_last_movement_input := 0.0
+var wasd_is_showing := false
+
+func _ready():
+	$WASDLabel.modulate = Color(1,1,1,0)
+
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if direction:
+		seconds_from_last_movement_input = 0
 		velocity += direction * ACCELERATION
 		
 		if velocity.length_squared() > MAX_SPEED ** 2:
 			velocity = velocity.limit_length(MAX_SPEED)
+		
+		if wasd_is_showing:
+			hide_wasd()
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECELLERATION)
 		velocity.y = move_toward(velocity.y, 0, DECELLERATION)
+		seconds_from_last_movement_input += delta
 
 	move_and_slide()
 	if velocity.length_squared() > MAX_SPEED / 4:
 		$AnimationPlayer.play("hop")
+	
+	if seconds_from_last_movement_input >= 3.0 and not wasd_is_showing:
+		show_wasd()
+
+func show_wasd():
+	var tween = get_tree().create_tween()
+	tween.tween_property($WASDLabel, "modulate", Color(1,1,1,1), 0.75)
+	wasd_is_showing = true
+
+func hide_wasd():
+	var tween = get_tree().create_tween()
+	tween.tween_property($WASDLabel, "modulate", Color(1,1,1,0), 0.15)
+	wasd_is_showing = false
