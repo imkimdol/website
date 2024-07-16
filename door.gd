65a8f23e15547463db63423a0c90fcc6e9222b33
imkimdol <kimdol@proton.me>
2024-07-16 11:25:01 -0700

    Add door warp animation

--- door.gd
+++ door.gd
@@ -2,6 +2,7 @@ class_name Door extends StaticBody2D
 
 @export var hyperlink := "https://harperkim.ca"
 @export var player: Player
+@export var cutscene_animation: AnimationPlayer
 var player_entered := false
 
 func _process(delta):
@@ -9,6 +10,10 @@ func _process(delta):
 		on_door_interact()
 
 func on_door_interact():
+	player.controllable = false
+	self.visible = false
+	cutscene_animation.play("warp_cutscene")
+	await cutscene_animation.animation_finished
 	Global.open_link(hyperlink)
 
 func _on_area_2d_body_entered(body):
