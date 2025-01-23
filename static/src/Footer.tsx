6f0c9d8f2f02d13d67f22065b317adefa2a28f86
imkimdol <kimdol@proton.me>
2025-01-23 09:13:33 -0800

    Small HTML change to footer images

--- static/src/Footer.tsx
+++ static/src/Footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
     <section className='footer'>
       <ul>
         <li><a href="https://github.com/imkimdol" target="_blank" rel="noreferrer">
-          <img src="/github.png" alt="Github"></img>
+          <img src="/github.png" alt="Github"/>
         </a></li>
         <li><a href="https://www.linkedin.com/in/harper-kim/" target="_blank" rel="noreferrer">
-          <img src="/linkedin.png" alt="LinkedIn"></img>
+          <img src="/linkedin.png" alt="LinkedIn"/>
         </a></li>
         <li><a href="mailto:contact@harperkim.ca" target="_blank" rel="noreferrer">
-          <img src="/email.png" alt="Email"></img>
+          <img src="/email.png" alt="Email"/>
         </a></li>
       </ul>
     </section>
