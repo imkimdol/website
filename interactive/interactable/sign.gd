095ecffb5e55323a637069ac6ac39bc267f16dd8
imkimdol <kimdol@proton.me>
2025-01-28 09:59:20 -0800

    Add castle interactions

--- interactive/interactable/sign.gd
+++ interactive/interactable/sign.gd
@@ -1,13 +1,23 @@
 class_name Sign extends StaticBody2D
 
 @export var text := "Text goes here!"
+@export var desc := ""
 
 var player_entered := false
 
 func _ready():
 	$InteractionLabel.modulate = Color(1,1,1,0)
-	var label = $InteractionLabel/HBoxContainer/Container/MarginContainer/Label
+	
+	var label = %Text
 	label.text = text
+	
+	var desc_label = %Desc
+	if desc == "":
+		desc_label.get_parent().queue_free()
+	else:
+		$InteractionLabel/FloatAnimation.stop()
+		$InteractionLabel/FloatAnimation.play("float", -1, 0.5)
+		desc_label.text = desc
 
 func _on_area_2d_body_entered(body):
 	player_entered = true
