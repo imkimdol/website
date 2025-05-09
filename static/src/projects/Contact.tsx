9f929ad0a2917e3f49fe896d68876c3b266c29a4
imkimdol <kimdol@proton.me>
2025-05-09 10:55:41 -0700

    Rearrange contact list

--- static/src/projects/Contact.tsx
+++ static/src/projects/Contact.tsx
@@ -10,14 +10,14 @@ export default function Contact() {
         </h3>
         <div className='icons'>
           <ul>
-            <li><a href="https://github.com/imkimdol" target="_blank" rel="noreferrer">
-              <img src="/images/icons/github.png" alt="Github"/>
+            <li><a href="mailto:contact@harperkim.ca" target="_blank" rel="noreferrer">
+              <img src="/images/icons/email.png" alt="Email"/>
             </a></li>
             <li><a href="https://www.linkedin.com/in/harper-kim/" target="_blank" rel="noreferrer">
               <img src="/images/icons/linkedin.png" alt="LinkedIn"/>
             </a></li>
-            <li><a href="mailto:contact@harperkim.ca" target="_blank" rel="noreferrer">
-              <img src="/images/icons/email.png" alt="Email"/>
+            <li><a href="https://github.com/imkimdol" target="_blank" rel="noreferrer">
+              <img src="/images/icons/github.png" alt="Github"/>
             </a></li>
           </ul>
         </div>
