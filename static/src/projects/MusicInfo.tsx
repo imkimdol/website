d588d2ee1cb1ce51c78527ffff9012e6d7e597c5
imkimdol <kimdol@proton.me>
2025-05-09 12:43:07 -0700

    Icon bounce on hover

--- static/src/projects/MusicInfo.tsx
+++ static/src/projects/MusicInfo.tsx
@@ -8,22 +8,22 @@ export default function MusicInfo() {
         <h3>
           Harper is producing music under the alias Kimdol.
         </h3>
-        <div className='icons'>
+        <div className="icons">
           <ul>
             <li><a href="https://open.spotify.com/artist/4KdWdsftuaGAGVw595eJNM" target="_blank" rel="noreferrer">
-              <img src="/images/icons/spotify.png" alt="Github"/>
+              <img src="/images/icons/spotify.png" alt="Spotify"/>
             </a></li>
             <li><a href="https://music.apple.com/artist/kimdol/1602798959" target="_blank" rel="noreferrer">
-              <img src="/images/icons/apple-music.png" alt="LinkedIn"/>
+              <img src="/images/icons/apple-music.png" alt="Apple Music"/>
             </a></li>
             <li><a href="https://www.youtube.com/@imkimdol" target="_blank" rel="noreferrer">
-              <img src="/images/icons/youtube-music.png" alt="LinkedIn"/>
+              <img src="/images/icons/youtube-music.png" alt="YouTube Music"/>
             </a></li>
             <li><a href="https://kimdol.bandcamp.com/" target="_blank" rel="noreferrer">
-              <img src="/images/icons/bandcamp.png" alt="LinkedIn"/>
+              <img src="/images/icons/bandcamp.png" alt="Bandcamp"/>
             </a></li>
             <li><a href="https://kimdol.com" target="_blank" rel="noreferrer">
-              <img src="/images/icons/website.png" alt="Email"/>
+              <img src="/images/icons/website.png" alt="Website"/>
             </a></li>
           </ul>
         </div>
